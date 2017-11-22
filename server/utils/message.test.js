95e95f9d643d0bf23679e5fbea2b47890ca46f33
Ricardo Yuji Nagamatsu Nakashima <ricardoyuji@brq.com>
2017-11-22 15:17:12 -0200

    swsw

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require("expect");
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe("generateMessage", () => {
 	it('should generate correct message object', () => {
@@ -8,7 +8,22 @@ describe("generateMessage", () => {
 		var text = "Heu";
 		var message = generateMessage(from, text);
 
-		expect(message.createdAt).toBeA("number");
-		expect(message).toInclude({from, text});
+		expect(typeof message.createdAt).toBe("number");
+		expect(message).toMatchObject({from, text});
+	});
+});
+
+describe('generationLocationMessage', () => {
+	it('should generate correct location object', () => {
+		let from = 'Deb';
+		let latitude = 15;
+		var longitude = 19;
+		var url = 'https://www.google.com/maps?q=15,19';
+		let message = generateLocationMessage(from, latitude, longitude);
+
+		// expect(message.createdAt).toBeA('number');
+		expect(typeof message.createdAt).toBe('number');
+		expect(message).toMatchObject({from, url});
+
 	});
 });
