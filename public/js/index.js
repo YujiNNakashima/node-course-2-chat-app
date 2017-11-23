dd98c7893218ab8d6f8cdbfca07a241a42fc57ca
Ricardo Yuji Nagamatsu Nakashima <ricardoyuji@brq.com>
2017-11-23 12:03:19 -0200

    Dcd

--- public/js/index.js
+++ public/js/index.js
@@ -9,22 +9,42 @@ socket.on('disconected', function () {
 	});
 
 socket.on('newMessage', function(message) {
-	let formattedTime = moment(message.createdAt).format('h:mm a');
-	let li = $("<li></li>");
-	li.text(`${message.from} ${formattedTime}: ${message.text}`)
+	let formattedTime = moment(message.createdAt).format('h: mm a');
+	let template = $('#message-template').html();
+	let html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
+
+	$('#messages').append(html);
+
+	// let formattedTime = moment(message.createdAt).format('h:mm a');
+	// let li = $("<li></li>");
+	// li.text(`${message.from} ${formattedTime}: ${message.text}`)
 	
-	$('#messages').append(li);
+	// $('#messages').append(li);
 	});
 
 socket.on('newLocatonMessage', function(message) {
-	let formattedTime = moment(message.createdAt).format('h: mm a');
-	let li = $('<li></li');
-	let a = $('<a target="_blank">My current location</a>')
+	let formattedTime = moment(message.createdAt).format('h: mma');
+	let template =$("#location-message-template").html();
+	let html = Mustache.render(template, {
+		from: message.from,
+		url: message.url,
+		createdAt: formattedTime
+	});
 
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url);
-	li.append(a);
-	$('#messages').append(li);
+	$("#messges").append(html);
+
+
+	// let li = $('<li></li');
+	// let a = $('<a target="_blank">My current location</a>')
+
+	// li.text(`${message.from} ${formattedTime}: `);
+	// a.attr('href', message.url);
+	// li.append(a);
+	// $('#messages').append(li);
 });
 
 $("#message-form").on('submit', function(e) {
